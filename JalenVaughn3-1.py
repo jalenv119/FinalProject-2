2ca81508001a797109fc31d72304ae0c2b36920f
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-11-29 14:01:25 -0600

    Add files via upload

---
+++ JalenVaughn3-1.py
@@ -0,0 +1,54 @@
+import formulas 
+import sys
+num_arg = len(sys.argv)
+values = []
+
+def main():
+    if num_arg <= 1:
+        print('Need to provide operator.')
+        sys.exit(1)
+    elif num_arg <= 3:
+        print('Need to provide at least two values.')
+        sys.exit(1)
+    elif num_arg > 3:
+
+        op = sys.argv[1]
+        if op not in ('add' , 'subtract' , 'multiply', 'divide'):
+            print('Valid operator names (add, subtract, multiply, divide)')
+            sys.exit(1)
+
+
+        for arg in sys.argv[2:]:
+            try:
+                value = float(arg)
+                values.append(value)
+            except ValueError:
+                sys.exit(1)
+            
+       
+        if op == 'add':
+            result = formulas.add(values)
+        elif op == 'subtract':
+            result = formulas.subtract(values)
+        elif op == 'multiply':
+            result = formulas.multiply(values)
+        elif op == 'divide':
+            result = formulas.divide(values)
+
+        print(f'Answer = {result:.2f}')
+
+       
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
