fd7eae1b94b1697a5814dcebdd15d276e57afd75
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-12-08 18:19:35 +0000

    jv locked

--- logic.py
+++ logic.py
@@ -1,56 +1,82 @@
-import formulas 
-import sys
-import gui
+from PyQt6 import QtWidgets
+from gui import Ui_MainWindow
 
-num_arg = len(sys.argv)
-values = []
+class Calc:
+    def add(values):
+        neg_sum = 0
+        for num in values:
+            if num < 0:
+                neg_sum += num
+        return neg_sum
 
-def main():
-    if num_arg <= 1:
-        print('Need to provide operator.')
-        sys.exit(1)
-    elif num_arg <= 3:
-        print('Need to provide at least two values.')
-        sys.exit(1)
-    elif num_arg > 3:
+    def subtract(values):
+        pos_diff = None
+        for num in values:
+            if num > 0:
+                if pos_diff is None:
+                    pos_diff = num
+                else:
+                    pos_diff -= num
+        if pos_diff is None:
+            return 0
+        return pos_diff
 
-        op = sys.argv[1]
-        if op not in ('add' , 'subtract' , 'multiply', 'divide'):
-            print('Valid operator names (add, subtract, multiply, divide)')
-            sys.exit(1)
+    def multiply(values):
+        multi_sum = 1
+        no_zeros = False
+        for num in values:
+            if num != 0:
+                multi_sum *= num
+                no_zeros = True
+        if not no_zeros:
+            return 0
+        return multi_sum
 
+    def divide(values):
+        divi_sum = 1
+        zeros = 0
 
-        for arg in sys.argv[2:]:
+        for num in values:
+            if num == 0:
+                zeros += 1
+                if zeros > 1:
+                    print('Cannot divide by 0')
+                    raise ValueError('Cannot divide by 0') 
+            else:
+                divi_sum /= num
+
+        if zeros == 1:
+            return 0
+        return divi_sum
+
+class LogicController:
+    def __init__(self, ui):
+        self.ui = ui
+
+    def process_input(self, args):
+        num_arg = len(args)
+        values = []
+
+        if num_arg <= 1 or num_arg <= 3:
+            self.show_error('Invalid input.')
+            return
+
+        op = args[1]
+        if op not in ('add', 'subtract', 'multiply', 'divide'):
+            self.show_error('Valid operator names (add, subtract, multiply, divide)')
+            return
+
+        for arg in args[2:]:
             try:
                 value = float(arg)
                 values.append(value)
             except ValueError:
-                sys.exit(1)
-            
-       
-        if op == 'add':
-            result = formulas.add(values)
-        elif op == 'subtract':
-            result = formulas.subtract(values)
-        elif op == 'multiply':
-            result = formulas.multiply(values)
-        elif op == 'divide':
-            result = formulas.divide(values)
+                self.show_error('Invalid input.')
+                return
 
-        print(f'Answer = {result:.2f}')
+        result = getattr(Calc, op)(values)
+        expression = ' '.join(args[2:])
+        self.ui.textEdit.setPlainText(f'{expression} = {result:.2f}')
 
-       
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
-
-
+    def show_error(self, message):
+        QtWidgets.QMessageBox.critical(None, "Error", message)
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-<<<<<<< HEAD
-main
+<<<<<<< 
+from PyQt6 import QtWidgets
+from gui import Ui_MainWindow
+from logic import LogicController
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    main_window = Ui_MainWindow()
+    main_window.show()
+    logic_controller = LogicController()
+
+    # Connect button signals to methods in the MainWindow class
+    for button in main_window.centralwidget.findChildren(QtWidgets.QPushButton):
+        if button.text().isdigit() or button.text() in {'+', '-', '*', '/'}:
+            button.clicked.connect(main_window.on_button_clicked)
+
+    main_window.ClearButton.clicked.connect(main_window.clear_text)
+    main_window.SubmitButton.clicked.connect(main_window.submit_expression)
+
+    sys.exit(app.exec())
 =======
 
 
