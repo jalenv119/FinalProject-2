4f53d69af65d44758f37af98568fd89cba680640
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-12-08 19:26:23 +0000

    supa dupa boof

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 <<<<<<< 
 from PyQt6 import QtWidgets
-from gui import Ui_MainWindow
-from logic import LogicController
+from logic import *
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
+
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    main_window = Ui_MainWindow()
-    main_window.show()
-    logic_controller = LogicController()
-
-    # Connect button signals to methods in the MainWindow class
-    for button in main_window.centralwidget.findChildren(QtWidgets.QPushButton):
-        if button.text().isdigit() or button.text() in {'+', '-', '*', '/'}:
-            button.clicked.connect(main_window.on_button_clicked)
-
-    main_window.ClearButton.clicked.connect(main_window.clear_text)
-    main_window.SubmitButton.clicked.connect(main_window.submit_expression)
-
-    sys.exit(app.exec())
+    main()
+   
 =======
 
 
