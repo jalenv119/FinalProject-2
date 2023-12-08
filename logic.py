d0700461d3d90d68b3bb66d02dca6fd2d58a4d75
jalenv119 <150954380+jalenv119@users.noreply.github.com>
2023-12-08 21:38:42 +0000

    logic boiiii

--- logic.py
+++ logic.py
@@ -1,21 +1,70 @@
 from PyQt6.QtWidgets import *
-from gui import *
+from gui import Ui_MainWindow
 
 class Logic(QMainWindow, Ui_MainWindow):
+    pressed_buttons = []
+    
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.current_text = ''
 
+        self.Number0Button.clicked.connect(self.on_button_clicked)
+        self.Number1Button.clicked.connect(self.on_button_clicked)
+        self.Number2Button.clicked.connect(self.on_button_clicked)
+        self.Number3Button.clicked.connect(self.on_button_clicked)
+        self.Number4Button.clicked.connect(self.on_button_clicked)
+        self.Number5Button.clicked.connect(self.on_button_clicked)
+        self.Number6Button.clicked.connect(self.on_button_clicked)
+        self.Number7Button.clicked.connect(self.on_button_clicked)
+        self.Number8Button.clicked.connect(self.on_button_clicked)
+        self.Number9Button.clicked.connect(self.on_button_clicked)
+        self.AdditionButton.clicked.connect(self.on_button_clicked)
+        self.SubtractButton.clicked.connect(self.on_button_clicked)
+        self.MultiplyButton.clicked.connect(self.on_button_clicked)
+        self.DivideButton.clicked.connect(self.on_button_clicked)
+        self.ClearButton.clicked.connect(self.on_button_clicked)
+        self.SubmitButton.clicked.connect(self.on_button_clicked)
 
-    def add(values):
+    def on_button_clicked(self):
+        button = self.sender()
+        button_text = button.text()
+
+        if button_text == "CLEAR":
+            self.clear_text()
+        elif button_text == "SUBMIT":
+            self.evaluate_expression()
+        else:            
+            self.pressed_buttons.append(button_text)
+            self.update_text()
+
+    def clear_text(self):
+        self.pressed_buttons = []
+        self.update_text()
+
+    def evaluate_expression(self):
+        try:
+            expression = "".join(self.pressed_buttons)
+            result = eval(expression)
+            self.textEdit.clear()
+            self.textEdit.setPlainText(f'{result:.2f}')
+            self.textEdit.setFocus()
+            self.pressed_buttons = [str(result)]  # Store the result as a string
+        except Exception as e:
+            self.textEdit.setPlainText(str(e))
+            self.pressed_buttons = []
+
+    def update_text(self):
+        self.textEdit.setPlainText(" ".join(self.pressed_buttons))
+
+    def add(self, values):
         neg_sum = 0
         for num in values:
             if num < 0:
                 neg_sum += num
         return neg_sum
 
-    def subtract(values):
+    def subtract(self, values):
         pos_diff = None
         for num in values:
             if num > 0:
@@ -27,7 +76,7 @@ class Logic(QMainWindow, Ui_MainWindow):
             return 0
         return pos_diff
 
-    def multiply(values):
+    def multiply(self, values):
         multi_sum = 1
         no_zeros = False
         for num in values:
@@ -38,7 +87,7 @@ class Logic(QMainWindow, Ui_MainWindow):
             return 0
         return multi_sum
 
-    def divide(values):
+    def divide(self, values):
         divi_sum = 1
         zeros = 0
 
